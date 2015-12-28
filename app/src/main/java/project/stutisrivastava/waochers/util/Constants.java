f529ec9218ef3e3620842b3d5af00a34807cf471
saumyasinha023 <saumyasinha.23>
2015-12-28 09:59:22 +0530

    New class that will store all the constants used throughout the app

---
+++ app/src/main/java/project/stutisrivastava/waochers/util/Constants.java
@@ -0,0 +1,12 @@
+package project.stutisrivastava.waochers.util;
+
+/**
+ * Created by stutisrivastava on 27/12/15.
+ */
+public class Constants {
+    public static final String SHARED_PREFERENCES_NAME = "WaochersSharedPref";
+    public static final String FBLOGIN = "fblogin";
+    public static final String GOOGLELOGIN = "googlelogin";
+    public static final String NORMALLOGIN = "normallogin";
+    public static final String LOGINMETHOD = "loginmethod";
+}
