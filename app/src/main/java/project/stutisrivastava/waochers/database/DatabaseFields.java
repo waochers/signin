01f5444c6733313f08a0f96797d18891d75fe612
saumyasinha023 <saumyasinha.23>
2015-12-30 08:43:45 +0530

    I/f which defines all the db constants

--- app/src/main/java/project/stutisrivastava/waochers/database/DatabaseFields.java
+++ app/src/main/java/project/stutisrivastava/waochers/database/DatabaseFields.java
@@ -1,18 +1,12 @@
 package project.stutisrivastava.waochers.database;
 
 public interface DatabaseFields {
-
 	int DATABASE_VERSION = 1;
 	String TABLE_USER = "CustomerTbl";
     String DATABASE_NAME = "Waochers.db";
-
     String KEY_CUSTOMER_NO = "CustomerNo";
-
     String KEY_CUSTOMER_NAME = "customername";
-
     String KEY_CUSTOMER_EMAIL= "email";
     String KEY_CUSTOMER_PHONE = "phonenumber";
-
     String KEY_CUSTOMER_PASSWORD = "password";
-
 }
