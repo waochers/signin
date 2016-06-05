2ec96d5e63680cc5913060f8ebed9611d4bfc7ad
saumyasinha023 <saumyasinha023@gmail.com>
2016-06-05 19:12:42 +0530

    updated till feature scroll

--- app/src/main/java/project/stutisrivastava/waochers/ui/ShopViewActivity.java
+++ app/src/main/java/project/stutisrivastava/waochers/ui/ShopViewActivity.java
@@ -1,14 +1,21 @@
 package project.stutisrivastava.waochers.ui;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import project.stutisrivastava.waochers.R;
 
@@ -16,28 +23,66 @@ public class ShopViewActivity extends AppCompatActivity {
 
     CollapsingToolbarLayout collapsingToolbarLayout;
     ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_view);
         image = (ImageView) findViewById(R.id.image);
         image.setImageResource(R.drawable.images);
-        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbarLayout.setTitle("ShopViewActivity");
-        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.white));
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.MyTheme);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        HorizontalScrollView scrollView = (HorizontalScrollView) findViewById(R.id.horFeatures);
+
+        LinearLayout topLinearLayout = new LinearLayout(this);
+        topLinearLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        topLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        LinearLayout innerLinearLayout = null;
+        for (int i = 0; i < 15; i++) {
+            innerLinearLayout = new LinearLayout(this);
+            innerLinearLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            innerLinearLayout.setOrientation(LinearLayout.VERTICAL);
+            ImageView imageView = new ImageView(this);
+            imageView.setTag(i);
+            imageView.setImageResource(R.drawable.parking);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(150, 120));
+            imageView.setPadding(15, 0, 15, 5);
+            TextView text = new TextView(this);
+            text.setText("Parking");
+            text.setLayoutParams(new ViewGroup.LayoutParams(180, 300));
+            innerLinearLayout.addView(imageView);
+            innerLinearLayout.addView(text);
+            topLinearLayout.addView(innerLinearLayout);
+        }
+        scrollView.addView(topLinearLayout);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(mapIntent);
+                    }
+
+                }
+            }
+        });
         setPalette();
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
     private void setPalette() {
         Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
